10cc1b057326eefb4083d38c312bec231523e4ab
ramirezjpdf <ramirez.jpdf@ymail.com>
2017-12-22 16:38:24 -0200

    interchange loops j and k. It makes loop j be the outermost loop

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -3,8 +3,8 @@ const char* dgemm_desc = "My awesome dgemm.";
 void square_dgemm(const int M, const double *A, const double *B, double *C)
 {
     int i, j, k;
-    for (k = 0; k < M; ++k) {
-        for (j = 0; j < M; ++j) {
+    for (j = 0; j < M; ++j) {
+        for (k = 0; k < M; ++k) {
             for (i = 0; i < M; ++i) {
                 C[j*M+i] += A[k*M+i] * B[j*M+k];
             }
