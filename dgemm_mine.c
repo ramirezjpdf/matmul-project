4bc421b2ac0411186084bf707114cd676e45ead0
ramirezjpdf <ramirez.jpdf@ymail.com>
2017-12-27 12:19:14 -0200

    a little improve by make another level of blocking for j

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -3,29 +3,41 @@ const char *dgemm_desc = "My awesome dgemm.";
 
 #define MIN(A, B) A <= B ? A : B
 
-#ifndef BLOCK_SIZE_MINE
-#define BLOCK_SIZE_MINE ((int)64)
+#ifndef BLOCK_SIZE_JJJ
+#define BLOCK_SIZE_JJJ ((int)16)
+#endif
+
+#ifndef BLOCK_SIZE_JJ
+#define BLOCK_SIZE_JJ ((int)4)
+#endif
+
+#ifndef BLOCK_SIZE_KK
+#define BLOCK_SIZE_KK ((int)16)
 #endif
 
 void square_dgemm(const int M, const double *A, const double *B, double *C)
 {
-    int jj, kk;
-    int limitjj, limitkk;
+    int jjj, jj, kk;
+    int limitjjj, limitjj, limitkk;
     int j, k, i;
-    for (jj = 0; jj < M; jj += BLOCK_SIZE_MINE)
+    for (jjj = 0; jjj < M; jjj += BLOCK_SIZE_JJJ)
     {
-        for (kk = 0; kk < M; kk += BLOCK_SIZE_MINE)
+        for (kk = 0; kk < M; kk += BLOCK_SIZE_KK)
         {
-            limitjj = MIN((jj + BLOCK_SIZE_MINE), M);
-            limitkk = MIN((kk + BLOCK_SIZE_MINE), M);
+            limitjjj = MIN((jjj + BLOCK_SIZE_JJJ), M);
+            limitkk = MIN((kk + BLOCK_SIZE_KK), M);
 
-            for (j = jj; j < limitjj; ++j)
+            for (jj = jjj; jj < limitjjj; jj += BLOCK_SIZE_JJ)
             {
-                for (k = kk; k < limitkk; ++k)
+                limitjj = MIN(jj + BLOCK_SIZE_JJ, M);
+                for (j = jj; j < limitjj; ++j)
                 {
-                    for (i = 0; i < M; ++i)
+                    for (k = kk; k < limitkk; ++k)
                     {
-                        C[j * M + i] += A[k * M + i] * B[j * M + k];
+                        for (i = 0; i < M; ++i)
+                        {
+                            C[j * M + i] += A[k * M + i] * B[j * M + k];
+                        }
                     }
                 }
             }
